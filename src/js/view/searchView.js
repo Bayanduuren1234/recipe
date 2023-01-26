5f1804108cbce531ad906bd245b426f4fce29b7c
Bayanduuren <bayakabaynduuren@gmail.com>
2023-01-26 15:23:34 +0800

    hailt hiihed zoruilj spinner hiilee

--- src/js/view/searchView.js
+++ src/js/view/searchView.js
@@ -1,5 +1,32 @@
-var superAdd = (a, b) => a + b + 1000;
+import { elements } from "./base";
 
-export const add = (a, b) => superAdd(a, b);
-export let multiply = (a, b) => a * b;
-const id = 25;
+// private function
+const renderRecipe = (recipe) => {
+  const markup = `
+    <li>
+        <a class="results__link" href="#${recipe.recipe_id}">
+            <figure class="results__fig">
+                <img src="${recipe.image_url}" alt="Test">
+            </figure>
+            <div class="results__data">
+                <h4 class="results__name">${recipe.title}</h4>
+                <p class="results__author">${recipe.publisher}</p>
+            </div>
+        </a>
+    </li>
+  `;
+  // ul рүүгээ нэмнэ.
+  elements.searchResultList.insertAdjacentHTML("beforeend", markup);
+};
+
+export const clearSearchQuery = () => {
+  elements.searchInput.value = "";
+};
+export const clearSearchResult = () => {
+  elements.searchResultList.innerHTML = "";
+};
+
+export const getInput = () => elements.searchInput.value;
+export const renderRecipes = (recipes) => {
+  recipes.forEach(renderRecipe);
+};
